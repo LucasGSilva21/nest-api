8a9c9dc45b4bc7e10466665b0904c6625c1c585d
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-06-25 14:25:05 -0300

    refactor: move create user

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 import { LoginDto } from './dtos/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() createUser: CreateUserDto) {
+    return this.authService.register(createUser);
+  }
 
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 import { LoginDto } from './dtos/login.dto';
 
 @Injectable()
@@ -9,6 +10,10 @@ export class AuthService {
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
   ) {}
+
+  async register(createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
 
   async login(loginDto: LoginDto) {
     const user = await this.usersService.findOneByCriteria({
